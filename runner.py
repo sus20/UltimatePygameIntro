e093f507cab9ac259add64f65df55581e9468e22
sushma <a01363028@unet.univie.ac.at>
2023-09-14 21:34:50 +0200

    feat: implementedBasicAnmationsCoverImageWithRectangles

--- runner.py
+++ runner.py
@@ -9,11 +9,17 @@ test_font = pygame.font.Font(
     'font/Pixeltype.ttf', 50)  # (font type, font size)
 
 
-sky_surface = pygame.image.load('graphics/sky.png')  # (w,h)
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/sky.png').convert()  # (w,h)
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+# (text, antiAliasing, color)
+text_surface = test_font.render('My game', False, 'Black').convert()
 
-# (text, anti aliasing, color)
-text_surface = test_font.render('My game', False, 'Black')
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom=(600, 300))
+
+player_surf = pygame.image.load(
+    'graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(bottomright=(80, 300))
 
 
 while True:
@@ -25,6 +31,11 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))   # (from left, from top-down )
     screen.blit(text_surface, (300, 50))
+
+    screen.blit(snail_surf, snail_rect)
+
+    player_rect.left += 1
+    screen.blit(player_surf, player_rect)
 
     pygame.display.update()
     clock.tick(60)
