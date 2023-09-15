d8e01c8a605cea6c36a02f4d7cb2d4bbe7c6f237
sushma <a01363028@unet.univie.ac.at>
2023-09-15 10:13:35 +0200

    feat: drewWithrectanglesEllipse&Line

--- runner.py
+++ runner.py
@@ -15,7 +15,7 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 # (text, antiAliasing, color)
 score_surf = test_font.render('My game', False, 'Black')
-score_rect = score_surf.get_rect(bottomright=(600, 300))
+score_rect = score_surf.get_rect(bottomright=(400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom=(600, 300))
@@ -37,7 +37,11 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))   # (from left, from top-down )
-    screen.blit(score_surf, (300, 50))
+    pygame.draw.rect(screen, 'Pink', score_rect)
+    pygame.draw.rect(screen, 'Pink', score_rect, 10)
+    pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50, 200, 100, 100))
+    pygame.draw.line(screen, 'Gold', (0, 0), pygame.mouse.get_pos(), 10)
+    screen.blit(score_surf, score_rect)
 
     snail_rect.x -= 4
     if snail_rect.right <= 0:
