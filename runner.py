9f2efee08feb389d06f7f72c341b041bda63f547
sushma <a01363028@unet.univie.ac.at>
2023-09-15 11:33:36 +0200

    feat: CheckedTheCollisionBetweenPlayerAndFloor&movePlayerUpIfCollision

--- runner.py
+++ runner.py
@@ -31,16 +31,13 @@ while True:
             pygame.quit()
             exit()
 
-        if event.type == pygame.MOUSEMOTION:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if player_rect.collidepoint(event.pos):
-                print('collision')
+                player_gravity = -20
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 player_gravity = -20
-
-        if event.type == pygame.KEYUP:
-            print('Key UP')
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))   # (from left, from top-down )
@@ -56,18 +53,10 @@ while True:
     # Player
     player_gravity += 1
     player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
+
     screen.blit(player_surf, player_rect)
-
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #     print('jump')
-
-    # if player_rect.colliderect(snail_rect):
-    #     print('collision')
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
