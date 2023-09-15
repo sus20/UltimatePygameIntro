6dae8bf23eae395392f37811490918e533ad0a22
sushma <a01363028@unet.univie.ac.at>
2023-09-15 10:22:04 +0200

    feat: setupColorWithRGBOrHex

--- runner.py
+++ runner.py
@@ -14,7 +14,7 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 
 # (text, antiAliasing, color)
-score_surf = test_font.render('My game', False, 'Black')
+score_surf = test_font.render('My game', False, (64, 64, 64))
 score_rect = score_surf.get_rect(bottomright=(400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -37,10 +37,8 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))   # (from left, from top-down )
-    pygame.draw.rect(screen, 'Pink', score_rect)
-    pygame.draw.rect(screen, 'Pink', score_rect, 10)
-    pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50, 200, 100, 100))
-    pygame.draw.line(screen, 'Gold', (0, 0), pygame.mouse.get_pos(), 10)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
     screen.blit(score_surf, score_rect)
 
     snail_rect.x -= 4
