130b5978e8408f02ac395a840cbe2539a679afae
sushma <a01363028@unet.univie.ac.at>
2023-09-18 09:27:16 +0200

    feat: addingFlyEnemy

--- runner.py
+++ runner.py
@@ -17,7 +17,11 @@ def obstacle_movement(obstacle_list):
         for obstacle_rect in obstacle_list:
             obstacle_rect.x -= 5
 
-            screen.blit(snail_surf, obstacle_rect)
+            if obstacle_rect.bottom == 300:
+                screen.blit(snail_surf, obstacle_rect)
+            else:
+                screen.blit(fly_surf, obstacle_rect)
+
         obstacle_list = [
             obstacle for obstacle in obstacle_list if obstacle.x > -100]
 
@@ -47,7 +51,7 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 # Obstacles
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(bottomright=(600, 300))
+fly_surf = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
 
 obstacle_rect_list = []
 
@@ -89,12 +93,15 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                snail_rect.left = 800
                 start_time = int(pygame.time.get_ticks()/1000)
 
         if event.type == obstacle_timer and game_active:
-            obstacle_rect_list.append(snail_surf.get_rect(
-                bottomright=(randint(900, 1100), 300)))
+            if randint(0, 2):
+                obstacle_rect_list.append(snail_surf.get_rect(
+                    bottomright=(randint(900, 1100), 300)))
+            else:
+                obstacle_rect_list.append(snail_surf.get_rect(
+                    bottomright=(randint(900, 1100), 210)))
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
@@ -121,8 +128,7 @@ while True:
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
         # collision
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+
     else:
         screen.fill((94, 129, 162))
         screen.blit(player_stand, player_stand_rect)
