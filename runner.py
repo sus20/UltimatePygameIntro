515234c81d6a343898956d81a285621d23592e0d
sushma <a01363028@unet.univie.ac.at>
2023-09-19 23:12:56 +0200

    feat: addingMusic

--- runner.py
+++ runner.py
@@ -22,10 +22,14 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom=(80, 300))
         self.gravity = 0
 
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(0.5)
+
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
             self.gravity = -20
+            self.jump_sound.play()
 
     def apply_gravity(self):
         self.gravity += 1
@@ -113,6 +117,9 @@ test_font = pygame.font.Font(
 game_active = False
 start_time = 0
 score = 0
+bg_music = pygame.mixer.Sound('audio/music.wav')
+bg_music.set_volume(0.1)
+bg_music.play(loops=-1)
 
 # Groups
 player = pygame.sprite.GroupSingle()
